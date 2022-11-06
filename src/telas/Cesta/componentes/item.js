e70cbd9978f3a294a9d3ca9af0363a6ac12ec29f
Lucas Ilussencio da Silva <69581917+ilussencio@users.noreply.github.com>
2022-11-06 16:34:35 -0300

    Iniciando repositorio

---
+++ src/telas/Cesta/componentes/item.js
@@ -0,0 +1,33 @@
+import react from 'react';
+import { Image, View, StyleSheet } from 'react-native';
+import Texto from "../../../componentes/Texto";
+
+export default function Item({ item: { nome, imagem } }){
+    return <View key={nome} style={estilos.item}>
+            <Image source={imagem} style={estilos.imagem}/>
+            <Texto style={estilos.nome}>{nome}</Texto>
+        </View>
+}
+
+const estilos = StyleSheet.create({
+    item:{
+        flexDirection: "row",
+        borderBottomWidth: 1,
+        borderBottomColor: "#ececec",
+        paddingVertical: 16,
+        alignItems: "center",
+        paddingVertical: 16,
+        marginHorizontal: 16
+    }, 
+    imagem:{
+        width: 46,
+        height: 46
+    },
+    nome:{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color: "#464646"
+    }
+
+});
